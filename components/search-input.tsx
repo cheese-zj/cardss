133b993c77b779f111b1d5284b06c75275b5197f
James Zhao <jzha0385@uni.sydney.edu.au>
2024-06-09 20:45:21 +1000

    deploy2

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -39,9 +39,7 @@ export const SearchInput = () => {
 
 
     return (
-        <div
-        className="relative"
-        >
+        <div className="relative">
             <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
             <Input placeholder="Search" className="pl-10 bg-primary/10" onChange={onChange} value={value} />
         </div>
